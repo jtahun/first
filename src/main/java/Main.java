825a9bca8ea1839a0bf183ab7b6a18c3e0c42849
igor <jtahun@bk.ru>
2020-03-17 23:59:21 +0600

    first on the first

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+  public  static void main(String[]args){
+    try (HasResource1 res1 = new HasResource1()) {
+      System.out.println("make some action.");
+      throw new RuntimeException();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+    
+    
+    //System.out.println("Hello Java!!!");
+  }
+}
+
+class HasResource1 implements AutoCloseable {
+  public  HasResource1(){
+    System.out.println("get resouce1");
+  }
+  public void free(){
+    System.out.println("Free resource1");    
+  }
+  @Override
+  public void close(){
+    free();
+  }
+}
+
+public BeanGetter<JdbcAcces> jdbcAccess;
+
+private List<SomeClass> getSomeFunc(String one, String two){
+  return jdbcAccess.get().execute(new ConnectionCallback<List<SomeClass>>(){    //... .execute(new OtherClass
+    @Override 
+      public List<SomeClass> doInConnection(Connection connection) throws Exception{
+        return null;
+    }
+        
+  });
+}
+
+class OtherClass implements ConnectionCallback<List<SomeClass>>{
+  @Override
+    public List<SomeClass> doInConnection(Connection connection) throws Exception{
+      
+  }
+}
